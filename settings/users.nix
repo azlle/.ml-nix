91733cd2273a1fedc740bfd47d51b20f16030843
Azlle <moxmo2@pm.me>
2025-05-18 18:48:33 +0900

    Edit users.nix

--- settings/users.nix
+++ settings/users.nix
@@ -15,4 +15,10 @@
   };
 
   programs.fish.enable = true;
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
 }
