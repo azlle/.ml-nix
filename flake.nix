03d7e83ff676a5e6eefe377c0368d7646fb14450
Azlle <moxmo2@pm.me>
2025-05-18 21:39:41 +0900

    Edit users.nix

--- flake.nix
+++ flake.nix
@@ -4,13 +4,15 @@
   inputs = {
     #nix flake init -t templates#fullでFlakeの全構文が見れます
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, nixos-hardware, ... }@inputs: {
     nixosConfigurations.necrofantasia = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+	nixos-hardware.nixosModules.asus-zephyrus-ga503
       ];
     };
   };
--- settings/users.nix
+++ settings/users.nix
@@ -10,7 +10,7 @@
     openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHh60Pl9Y6ns/1cNY6kZC4AF/M1yXwbWL3OibsRSdp6X NixOS" ];
     extraGroups = [ "wheel" ];
     packages = with pkgs; [
-    #  thunderbird
+      protonup-qt
     ];
   };
 
