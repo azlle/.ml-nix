d7fd497e3a82c4f24a479d281002e31f178e2702
Azlle <moxmo2@pm.me>
2025-05-18 17:26:13 +0900

    Change GRUB device

--- configuration.nix
+++ configuration.nix
@@ -17,7 +17,7 @@
 
   # Bootloader.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/nvme0n1";
   boot.loader.grub.useOSProber = true;
 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
