cc78baf39c9c2496a6627ea0ff5a9c78c93953d5
Azlle <moxmo2@pm.me>
2025-05-22 07:12:09 +0900

    configuration.nixのimportsをsettingsディレクトリのdefault.nixを読む形で簡略化

--- configuration.nix
+++ configuration.nix
@@ -8,19 +8,12 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./settings/users.nix
-      ./settings/fonts.nix
-      ./settings/networks.nix
-      ./settings/audios.nix
-      ./settings/i18n.nix
-      ./settings/powers.nix
+      ./settings
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
@@ -48,26 +41,17 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Install firefox.
   programs = {
     firefox = {
       enable = true;
     };
   };
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     ghostty
     git
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
@@ -78,22 +62,13 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 
   nix = {
     settings = {
---
+++ settings/default.nix
@@ -0,0 +1,11 @@
+# default.nix
+{
+  imports = [
+    ./audios.nix
+    ./fonts.nix
+    ./i18n.nix
+    ./networks.nix
+    ./powers.nix
+    ./users.nix
+  ];
+}
