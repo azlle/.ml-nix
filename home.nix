3ec618198f04c50ae4b7426b4d64912dec29e263
Azlle <moxmo2@pm.me>
2025-05-23 04:04:40 +0900

    home.fileでartworksを配置

--- home.nix
+++ home.nix
@@ -11,4 +11,15 @@
     homeDirectory = "/home/${username}";
     stateVersion = "25.05";
   };
+
+  home.file = {
+    "eeshta_wallpaper.png" = {
+      target = "Pictures/eeshta_wallpaper.png";
+      source = ./artworks/nix-wallpaper-simple-dark-gray_mellomixed.png;
+    };
+    "eeshta_icon.png" = {
+      target = "Pictures/eeshta_icon.png";
+      source = ./artworks/IMG_4900_foricon.png;
+    };
+  };
 }
