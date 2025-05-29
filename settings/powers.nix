60ca2c77bc0b26d7697309e8ca146b6425a37649
Azlle <moxmo2@pm.me>
2025-05-30 03:20:17 +0900

    Edit powers.nix

--- settings/powers.nix
+++ settings/powers.nix
@@ -5,7 +5,7 @@
   services.power-profiles-daemon.enable = false; # asusctl need true
 
   services.tlp = {
-    enable = false;
+    enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
@@ -16,7 +16,13 @@
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 50;
+      CPU_MAX_PERF_ON_BAT = 70;
+
+      CPU_BOOST_ON_AC = 1;
+      CPU_BOOST_ON_BAT = 0;
+
+      RUNTIME_PM_ON_AC = "on";
+      RUNTIME_PM_ON_BAT = "on";
 
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 20; # 40 and below it starts to charge
