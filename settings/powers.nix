90f9f1eadcf72c9be07b2d62b5fc5fddccb31002
Azlle <moxmo2@pm.me>
2025-06-03 12:42:03 +0900

    powers.nixを簡略化

--- settings/powers.nix
+++ settings/powers.nix
@@ -2,36 +2,17 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.power-profiles-daemon.enable = false; # asusctl need true
-
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "schedutil";
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
 
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
       CPU_MAX_PERF_ON_BAT = 70;
-
-      CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
 
-      RUNTIME_PM_ON_AC = "on";
-      RUNTIME_PM_ON_BAT = "on";
-
-      #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 20; # 40 and below it starts to charge
       STOP_CHARGE_THRESH_BAT0 = 60; # 80 and above it stops charging
     };
-  };
-
-  services.asusd = {
-    enable = false;
-    enableUserService = false;
   };
 }
