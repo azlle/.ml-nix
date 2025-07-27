b41f90c46b3d20a226628972c00a352e7104ed86
Azlle <moxmo2@pm.me>
2025-07-27 17:53:10 +0900

    Edit: niriのconfig.kdlを調整, lyからgreetdへ変更, ewwを試してみる

--- modules/system/xserver.nix
+++ modules/system/xserver.nix
@@ -5,11 +5,18 @@
   programs.niri.enable = true;
   programs.niri.package = pkgs.niri-unstable;
 
-  programs.waybar.enable = true;
-
   services = {
     hypridle.enable = true;
-    displayManager.ly.enable = true;
+    displayManager.ly.enable = false;
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd niri-session";
+          user = "greeter";
+        };
+      };
+    };
   };
 
   niri-flake.cache.enable = true;
@@ -21,11 +28,16 @@
   };
 
   environment.systemPackages = with pkgs; [
+    greetd.tuigreet
+    waybar
+    eww
     xwayland-satellite
+    glib # niriswitcherにgdbusが必要
     niriswitcher
     rofi-wayland
     swww
     wl-clipboard
+    brightnessctl
     cliphist
     xdg-utils
     gnome-themes-extra
