511caafbf08f6c172ee5186c681d8be288449a4e
Azlle <moxmo2@pm.me>
2025-10-23 08:42:21 +0900

    Edit: ルーター機能が正常動作するように修正

--- modules/system/networks.nix
+++ modules/system/networks.nix
@@ -4,118 +4,116 @@
   options = {
     miyana.hotspot.enable = mkEnableOption "Enable Hotspot: ml_haukanruri";
   };
-
+  
   config = {
     networking = mkMerge [
+      # 共通設定
       {
         networkmanager.enable = false;
         useDHCP = false;
+        hostName = "necrofantasia";
         nameservers = [ "8.8.8.8" ];
         extraHosts = "127.0.0.1 suki-kira.com";
-        
         firewall = {
           enable = true;
-
           allowPing = true;
           checkReversePath = "loose";
-
           allowedTCPPorts = [ 22 47984 47989 47990 48010 ];
-          
           allowedUDPPortRanges = [
             { from = 47998; to = 48000; }
             { from = 8000; to = 8010; }
           ];
-          
-          # virbr0 for QEMU, VIRT
           trustedInterfaces = [ "virbr0" ];
         };
-
-        hostName = "necrofantasia";
       }
-
+      
+      # 通常モード（無線接続）
       (mkIf (!config.miyana.hotspot.enable) {
         wireless = {
           enable = true;
           networks."Buffalo-A-DF70".pskRaw = "***REMOVED***";
         };
-
-        interfaces.wlp4s0 = {
-          ipv4.addresses = [{
-            address = "192.168.1.178";
-            prefixLength = 24;
-          }];
-        };
-
+        interfaces.wlp4s0.ipv4.addresses = [{
+          address = "192.168.1.178";
+          prefixLength = 24;
+        }];
         defaultGateway = {
           address = "192.168.1.1";
           interface = "wlp4s0";
         };
       })
-
+      
+      # ホットスポットモード（有線→無線配信）
       (mkIf config.miyana.hotspot.enable {
         wireless.enable = false;
+        
+        # 有線インターフェース（インターネット側）
         interfaces.enp3s0 = {
+          useDHCP = false;
           ipv4.addresses = [{
             address = "192.168.1.178";
             prefixLength = 24;
           }];
         };
-
+        
+        # 無線インターフェース（ホットスポット側）
         interfaces.wlp4s0 = {
+          useDHCP = false;
           ipv4.addresses = [{
             address = "192.168.14.1";
             prefixLength = 24;
           }];
         };
-
+        
+        defaultGateway = {
+          address = "192.168.1.1";
+          interface = "enp3s0";
+        };
+        
+        # NAT設定
         nat = {
           enable = true;
           externalInterface = "enp3s0";
           internalInterfaces = [ "wlp4s0" ];
         };
-
-        defaultGateway = {
-          address = "192.168.1.1";
-          interface = "enp3s0";
-        };
+        
+        # ファイアウォールでホットスポット通信を許可
+        firewall.trustedInterfaces = [ "virbr0" "wlp4s0" ];
       })
     ];
-
+    
+    # ホットスポットサービス
     services.hostapd = mkIf config.miyana.hotspot.enable {
       enable = true;
       radios.wlp4s0 = {
-        band = "5g";
-        channel = 36;
+        band = "2g";
+        channel = 11;
         countryCode = "JP";
         networks.wlp4s0 = {
           ssid = "ml_haukanruri";
           authentication = {
             mode = "wpa2-sha256";
-            wpaPasswordFile = "/home/eeshta/.dotfiles/credentials/hkrr_pswd";
+            wpaPassword = "***REMOVED***";
+            # wpaPasswordFile = "/home/eeshta/.dotfiles/credentials/hkrr_pswd";
           };
         };
       };
     };
-
+    
     services.dnsmasq = mkIf config.miyana.hotspot.enable {
       enable = true;
       settings = {
         interface = "wlp4s0";
         bind-interfaces = true;
-        dhcp-range = ["192.168.14.10,192.168.14.30,24h"];
-        dhcp-option = ["3,192.168.14.1" "6,192.168.14.1"];
-        server = ["8.8.8.8" "8.8.4.4"];
-        address = [
-          "/suki-kira.com/0.0.0.0"
-        ];
+        dhcp-range = [ "192.168.14.10,192.168.14.30,24h" ];
+        dhcp-option = [ "3,192.168.14.1" "6,192.168.14.1" ];
+        server = [ "8.8.8.8" "8.8.4.4" ];
+        address = [ "/suki-kira.com/0.0.0.0" ];
       };
     };
-
+    
     boot.kernel.sysctl = mkIf config.miyana.hotspot.enable {
       "net.ipv4.ip_forward" = 1;
     };
   };
 }
-# Configure network proxy if necessary
-# networking.proxy.default = "http://user:password@proxy:port/";
-# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
