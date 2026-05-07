199fb14807031045ec3e52999d9277955962aefd
Azlle <moxmo2@pm.me>
2026-05-07 12:53:13 +0900

    refactor(.config/default.nix): リポジトリ名の変更を適用

--- hm_modules/.config/default.nix
+++ hm_modules/.config/default.nix
@@ -3,9 +3,9 @@
 
 {
   xdg.configFile = {
-    "niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hm_modules/.config/niri_config.kdl";
-    "niri/zephyrus.xkb".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hm_modules/.config/niri_zephyrus.xkb";
-    "niriswitcher/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hm_modules/.config/niris_config.toml";
-    "rofi/config.rasi".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/hm_modules/.config/rofi_config.rasi";
+    "niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix_ml/hm_modules/.config/niri_config.kdl";
+    "niri/zephyrus.xkb".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix_ml/hm_modules/.config/niri_zephyrus.xkb";
+    "niriswitcher/config.toml".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix_ml/hm_modules/.config/niris_config.toml";
+    "rofi/config.rasi".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix_ml/hm_modules/.config/rofi_config.rasi";
   };
 }
