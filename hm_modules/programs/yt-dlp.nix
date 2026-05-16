fe03c73cb45596b666642996d74051251f2a9dcf
Azlle <moxmo2@pm.me>
2026-05-17 08:15:39 +0900

    feat(yt-dlp.nix): Dear13ro.用に.dotfiles_debから緊急輸入

--- hm_modules/programs/yt-dlp.nix
+++ hm_modules/programs/yt-dlp.nix
@@ -1,20 +1,66 @@
 # yt-dlp.nix
-{ ... }:
+# { ... }:
+
+# {
+#   programs.yt-dlp = {
+#     enable = false;
+#     extraConfig = ''
+#       # --cookies-from-browser firefox
+#       --output "~/Videos/%(extractor_key)s/%(uploader_id)s/%(timestamp>%Y-%m-%d_%H-%M)s_%(title)s_%(id)s.%(ext)s"
+#     '';
+#   };
+# 
+#   xdg.configFile."yt-dlp/config".text = ''
+#     --cookies-from-browser firefox
+#     -f "bv*+ba/b"
+#     -o "/mnt/melchior/Videos/%(extractor_key)s/%(uploader_id)s/%(timestamp>%Y-%m-%d_%H-%M)s_%(title)s_%(id)s.%(ext)s"
+#     --embed-thumbnail
+#     --merge-output-format mkv
+#   '';
+# }
+
+# yt-dlp.nix
+{ pkgs, lib, ... }:
+
+let
+  yt-dlp-ejs = pkgs.python3Packages.yt-dlp-ejs.overrideAttrs (finalAttrs: oldAttrs: {
+    version = "0.8.0";
+    src = pkgs.fetchFromGitHub {
+      owner = "yt-dlp";
+      repo = "ejs";
+      rev = finalAttrs.version;
+      # hash = lib.fakeHash;
+      hash = "sha256-+tOA9sPk0BGJHFQCoAC8y5Bz3UcjgIPDQ8WDPkRlW5k=";
+    };
+  });
+
+  yt-dlp = pkgs.yt-dlp.overrideAttrs (finalAttrs: oldAttrs: {
+    version = "2026.03.17";
+    src = pkgs.fetchFromGitHub {
+      owner = "yt-dlp";
+      repo = "yt-dlp";
+      rev = finalAttrs.version;
+      # hash = lib.fakeHash;
+      hash = "sha256-A4LUCuKCjpVAOJ8jNoYaC3mRCiKH0/wtcsle0YfZyTA=";
+    };
+    propagatedBuildInputs = map (dep:
+      if dep.pname or "" == "yt-dlp-ejs" then yt-dlp-ejs else dep
+    ) oldAttrs.propagatedBuildInputs;
+  });
+in
 
 {
-  programs.yt-dlp = {
-    enable = false;
-    extraConfig = ''
-      # --cookies-from-browser firefox
-      --output "~/Videos/%(extractor_key)s/%(uploader_id)s/%(timestamp>%Y-%m-%d_%H-%M)s_%(title)s_%(id)s.%(ext)s"
-    '';
-  };
+  home.packages = [ yt-dlp ];
 
   xdg.configFile."yt-dlp/config".text = ''
+    -o "$HOME/ydl_dest/%(extractor_key)s/%(uploader_id)s/%(timestamp>%Y-%m-%d_%H-%M)s_%(title)s_%(id)s.%(ext)s"
     --cookies-from-browser firefox
-    -f "bv*+ba/b"
-    -o "/mnt/melchior/Videos/%(extractor_key)s/%(uploader_id)s/%(timestamp>%Y-%m-%d_%H-%M)s_%(title)s_%(id)s.%(ext)s"
+    --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:150.0) Gecko/20100101 Firefox/150.0"
     --embed-thumbnail
-    --merge-output-format mkv
+    --embed-metadata
+    --sleep-requests 5
+    --sleep-interval 5
+    --max-sleep-interval 10
   '';
 }
+
