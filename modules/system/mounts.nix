e441cae403e83507e444e9236978d50f64fb7a6c
Azlle <moxmo2@pm.me>
2026-05-17 08:16:53 +0900

    feat(mounts.nix): yamaxanaduを追加

--- modules/system/mounts.nix
+++ modules/system/mounts.nix
@@ -25,6 +25,24 @@
     ];
   };
 
+  fileSystems."/mnt/yamaxanadu" = {
+    device = "//192.168.11.11/main";
+    fsType = "cifs";
+    options = [
+      "credentials=/etc/nixos/smb.yamaxanadu"
+      "uid=1000"
+      "gid=100"
+      "iocharset=utf8"
+      "vers=3.1.1"
+      "noauto"
+      "nofail"
+      "x-systemd.automount"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5"
+      "x-systemd.mount-timeout=5"
+    ];
+  };
+
   fileSystems."/mnt/melchior" = {
     device = "/dev/disk/by-uuid/154ee557-cfa5-44a3-82e8-75edbcc83f8b";
     fsType = "btrfs";
