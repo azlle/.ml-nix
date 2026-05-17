984c3ecbbb6da2f906ec30d3ba661d4dffa424f8
Azlle <moxmo2@pm.me>
2026-05-17 18:49:03 +0900

    feat(mounts.nix): melchiorとsaigyoujiの自動マウント設定を削除

--- modules/system/mounts.nix
+++ modules/system/mounts.nix
@@ -42,41 +42,4 @@
       "x-systemd.mount-timeout=5"
     ];
   };
-
-  fileSystems."/mnt/melchior" = {
-    device = "/dev/disk/by-uuid/154ee557-cfa5-44a3-82e8-75edbcc83f8b";
-    fsType = "btrfs";
-    options = [
-      "defaults"
-      "noatime"
-      "compress=zstd:3"
-      "ssd"
-      "discard=async"
-      "space_cache=v2"
-      "noauto"
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5"
-      "x-systemd.mount-timeout=5"
-    ];
-  };
-
-  fileSystems."/mnt/saigyouji" = {
-    device = "/dev/disk/by-uuid/969cfad8-3e8e-4f26-9591-3104eb2e71d8";
-    fsType = "btrfs";
-    options = [
-      "defaults"
-      "noatime"
-      "compress=zstd:3"
-      "autodefrag"
-      "space_cache=v2"
-      "noauto"
-      "nofail"
-      "x-systemd.automount"
-      "x-systemd.idle-timeout=60"
-      "x-systemd.device-timeout=5"
-      "x-systemd.mount-timeout=5"
-    ];
-  };
 }
