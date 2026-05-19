82b333d64b771c336375e155d33321cc9788d3da
Azlle <moxmo2@pm.me>
2026-05-20 00:51:25 +0900

    refactor(yt-dlp.nix): programs.yt-dlpに統合

--- hm_modules/yt-dlp.nix
+++ hm_modules/yt-dlp.nix
@@ -8,7 +8,6 @@ let
       owner = "yt-dlp";
       repo = "ejs";
       rev = finalAttrs.version;
-      # hash = lib.fakeHash;
       hash = "sha256-+tOA9sPk0BGJHFQCoAC8y5Bz3UcjgIPDQ8WDPkRlW5k=";
     };
   });
@@ -20,7 +19,6 @@ let
         owner = "yt-dlp";
         repo = "yt-dlp";
         rev = finalAttrs.version;
-        # hash = lib.fakeHash;
         hash = "sha256-A4LUCuKCjpVAOJ8jNoYaC3mRCiKH0/wtcsle0YfZyTA=";
       };
       propagatedBuildInputs = map (
@@ -39,16 +37,18 @@ let
 in
 
 {
-  home.packages = [ yt-dlp ];
+  programs.yt-dlp = {
+    enable = true;
+    package = yt-dlp;
 
-  xdg.configFile."yt-dlp/config".text = ''
-    -o "$HOME/ydl_dest/%(extractor_key)s/%(uploader_id)s/%(timestamp>%Y-%m-%d_%H-%M)s_%(title)s_%(id)s.%(ext)s"
-    --cookies-from-browser firefox:/mnt/c/Users/Eeshta/AppData/Roaming/Mozilla/Firefox/Profiles/907uf8a4.default-nightly
-    --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:150.0) Gecko/20100101 Firefox/150.0"
-    --embed-thumbnail
-    --embed-metadata
-    --sleep-requests 5
-    --sleep-interval 5
-    --max-sleep-interval 10
-  '';
+    settings = {
+      output = "$HOME/ydl_dest/%(extractor_key)s/%(uploader_id)s/%(timestamp>%Y-%m-%d_%H-%M)s_%(title)s_%(id)s.%(ext)s";
+      cookies-from-browser = cookiesFromBrowser;
+      embed-thumbnail = true;
+      embed-metadata = true;
+      sleep-requests = "5";
+      sleep-interval = "5";
+      max-sleep-interval = "10";
+    };
+  };
 }
